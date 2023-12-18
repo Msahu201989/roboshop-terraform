238ef09299d238e2d059119f12013b295fae8a51
Mukesh.Sahu <msahu201989@gmail.com>
2023-12-18 19:59:32 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -1,18 +1,8 @@
-#module "components" {
-#  source = "git::https://github.com/Msahu201989/tf-module-vpc.git"
-#
-#  for_each = var.vpc
-#  cidr     = each.value["cidr"]
-#  subnets   = each.value["subnets"]
-#}
-
-
-
 module "components" {
   source = "git::https://github.com/Msahu201989/tf-module-vpc.git"
 
   for_each = var.vpc
   cidr     = each.value["cidr"]
-  subnets  = each.value["subnets"]
-
+  subnets   = each.value["subnets"]
 }
+
